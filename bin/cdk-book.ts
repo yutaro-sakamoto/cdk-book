164c86e98b6d6845c23501de1bd7e05874f495f6
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-07-13 11:32:38 +0900

    [Add]: First commit

--- bin/cdk-book.ts
+++ bin/cdk-book.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkBookStack } from '../lib/cdk-book-stack';
+import { Chapter1IntroductionToIacAndAwsCdkStack } from '../lib/Chapter1IntroductionToIacAndAwsCdkStack';
 
 const app = new cdk.App();
-new CdkBookStack(app, 'CdkBookStack', {
+new Chapter1IntroductionToIacAndAwsCdkStack(app, 'Chapter1IntroductionToIacAndAwsCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
