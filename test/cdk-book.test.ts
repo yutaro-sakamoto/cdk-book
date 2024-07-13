e99b2b4289f0db46146b8ea75311c617033cd106
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-07-13 10:31:11 +0900

    Initial commit

---
+++ test/cdk-book.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkBook from '../lib/cdk-book-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-book-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkBook.CdkBookStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
