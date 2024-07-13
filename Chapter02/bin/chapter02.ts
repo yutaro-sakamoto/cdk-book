7ed6dd14535b781a5cc2c2fe4ffb400057572474
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-07-14 00:53:18 +0900

    [Add]: add checks by cdk-nag

--- Chapter02/bin/chapter02.ts
+++ Chapter02/bin/chapter02.ts
@@ -2,5 +2,9 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { WebStack } from "../lib/WebStack";
+import { Aspects } from "aws-cdk-lib";
+import { AwsSolutionsChecks } from "cdk-nag";
+
 const app = new cdk.App();
+Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
 new WebStack(app, "WebStack", {});
