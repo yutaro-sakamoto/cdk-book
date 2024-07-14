227702b06c989a6078828726e61790f6d0e643df
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-07-14 14:10:31 +0900

    [Add]: constructs/S3Bucket/index.ts

---
+++ Chapter02/lib/constructs/S3Bucket/index.ts
@@ -0,0 +1,21 @@
+import { RemovalPolicy } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+
+interface Props {
+    environment: string;
+}
+
+export class S3Bucket extends Construct {
+    public readonly bucket: Bucket;
+
+    constructor(scope: Construct, id: string, props: Props) {
+        super(scope, id);
+        const bucketName = props.environment == 'production' ? 'bucket-s3' : 'bucket-s3-dev';
+        this.bucket = new Bucket(scope, 'Bucket-S3', {
+            bucketName,
+            removalPolicy: RemovalPolicy.DESTROY,
+            publicReadAccess: true
+        })
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 これは、下記のリポジトリを参考に個人的な学習に使用するリポジトリです。
+
+
 https://github.com/PacktPublishing/AWS-CDK-in-Practice
